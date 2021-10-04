714dc4b81bb8d674750442cde9f471780aba782d
nthusualcoder <91885843+nthusualcoder@users.noreply.github.com>
2021-10-04 21:13:07 +0900

    Update prob1.js

--- week1/prob1.js
+++ week1/prob1.js
@@ -1,7 +1,7 @@
 let i, j, mul;
-for(i=1; i<9; i++){
-  for(j=1; j<9; j++){
+for(i=1; i<10; i++){
+  for(j=1; j<10; j++){
     mul = i * j;
-    console.log(i * j = mul\n);
+    console.log(i + ' * ' + j + ' = '+ mul);
     }
 }
