6b220ecae56d17f3bd38faceb32aa0fe98312bf7
JONGMIN KIM <91885843+nthusualcoder@users.noreply.github.com>
2021-10-11 22:47:39 +0900

    Update prob2.js

--- week2/prob2.js
+++ week2/prob2.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
 
+const textbook = {
+    Subject: "Vector Mechanics For Engineers",
+    Subtitle: "Statics and Dynamics",
+    Author: ["Ferdinand P. Beer", "E. Russell Johnston, Jr.", "David F. Mazurek", "Philip J. Cornwell", "Brian P. Self"]
+}
+
+const textbookJSON = JSON.stringify(textbook);
+
+fs.writeFileSync('textbook.json', textbookJSON, "utf8");
