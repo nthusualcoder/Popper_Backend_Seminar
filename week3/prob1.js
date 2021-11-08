904c86271d80d1c48b775b1f0d702de5c6bb995e
JONGMIN KIM <91885843+nthusualcoder@users.noreply.github.com>
2021-11-08 21:48:35 +0900

    Update prob1.js

--- week3/prob1.js
+++ week3/prob1.js
@@ -1,1 +1,21 @@
+const date_utils = require('date-utils');
+
+var dt = new Date();
+
+var d = dt.toFormat('YYYY-MM-DD HH24:MI:SS')
+
+const http = require('http')
+
+const server = http.createServer((req, res) =>{
+  res.write(d);
+  res.end();
+});
+
+server.listen(8080)
+server.on('listening', () => {
+    console.log("server is running on 8080 port.");
+})
+server.on('error', (error) => {
+    console.log("error occured", error);
+})
 
